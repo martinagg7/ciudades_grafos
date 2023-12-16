96e309219a2835bbc044f94f0cf8a8eb5063c10f
lmiel <lucia.mielgo@gmail.com>
2023-12-17 00:15:10 +0100

    Updated class grafo

--- claseGrafo.py
+++ claseGrafo.py
@@ -1,4 +1,72 @@
-class NodoCamino:#camino entre dos ciudades
+class Nodo:#Nodo
+    def __init__(self,nombre):
+        self.nombre=nombre
+        self.caminos = [] #Objetos tipo arista
+        # self.visitado=None
+    def insertar_id(self,id):
+        self.id = id
+      
+class Arista:#Arista
+    def __init__(self, nodo1, nodo2, distancia):
+        self.nodo1 = nodo1
+        self.nodo2 = nodo2
+        self.distancia = distancia
+
+class Grafo:
+    def __init__(self):
+        self.numero_de_nodos = 0
+        self.nodos = []
+    
+    def insertar_nodo(self,nodo):
+        nodo.insertar_id(self.numero_de_nodos)
+        self.numero_de_nodos += 1
+        self.nodos.append(nodo)
+    
+    def insertar_arista(self, nodo1, nodo2, distancia):
+        arista = Arista(nodo1, nodo2, distancia)
+        nodo1.caminos.append(arista)
+        nodo2.caminos.append(arista)
+        
+    def mostrar_grafo(self):
+        matriz_de_adyacencia = []
+        for _ in range(self.numero_de_nodos):
+            matriz_de_adyacencia.append([0] * self.numero_de_nodos)
+        
+        for nodo in self.nodos:
+            for arista in nodo.caminos:
+                matriz_de_adyacencia[arista.nodo1.id][arista.nodo2.id] = arista.distancia
+                matriz_de_adyacencia[arista.nodo2.id][arista.nodo1.id] = arista.distancia
+                
+        for fila in matriz_de_adyacencia:
+            print(fila)
+        
+
+espania = Grafo()
+
+ciudades = [Nodo('Madrid'), Nodo('Barcelona'), Nodo('Cadiz'), Nodo('Lugo')]
+for ciudad in ciudades:
+    espania.insertar_nodo(ciudad)
+espania.insertar_arista(ciudades[0],ciudades[1],626)
+espania.insertar_arista(ciudades[1],ciudades[2],1108)
+espania.insertar_arista(ciudades[0],ciudades[3],500)
+espania.insertar_arista(ciudades[1],ciudades[3],994)
+
+    
+espania.mostrar_grafo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""class NodoCamino:#camino entre dos ciudades
     def __init__(self,distancia,ciudad_destino):
         self.distancia=distancia
         self.ciudad_destino=ciudad_destino
@@ -54,4 +122,4 @@ class Grafociudades:#grafo de nuestras ciudades unidas
         if ciudad_origen is not None and ciudad_destino is not None:
             ciudad_origen.caminos.insertar(distancia,ciudad_destino)
             if not self.dirigido:# si no es dirigido puede haber camino de a->b pero no de b->a
-                ciudad_destino.caminos.insertar(distancia,ciudad_origen)
+                ciudad_destino.caminos.insertar(distancia,ciudad_origen)"""
---
+++ ruta_mas_corta.py
@@ -0,0 +1,2 @@
+from claseGrafo import *
+
