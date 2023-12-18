d68fc4fe31f90be4167f571a8b2ab0d772a5557a
Martina Garcia <martinagg@myuax.com>
2023-12-18 17:20:25 +0100

    .

---
+++ bst.py
@@ -0,0 +1,41 @@
+class NodoArbol:
+    def __init__(self, distancia, ciudades):
+        self.distancia = distancia
+        self.ciudades = ciudades #lista de ciudades
+        self.izquierda = None
+        self.derecha = None
+
+class Arbol:
+    def __init__(self):
+        self.raiz = None
+
+    def insertar(self, distancia, ciudades):
+        if self.raiz is None:# si el arbol esta vacio insertamos si no llamos a la función recursiva
+            self.raiz = Nodo(distancia, ciudades)
+        else:
+            self.insertar_recur(distancia, ciudades, self.raiz)
+
+    def insertar_recur(self, distancia, ciudades, nodo):
+        if distancia < nodo.distancia:
+            if nodo.izquierda is None:
+                nodo.izquierda = Nodo(distancia, ciudades)
+            else:
+                self.insertar_recur(distancia, ciudades, nodo.izquierda) #recorremos bst hasta encontrar su posición de forma recursiva
+        elif distancia > nodo.distancia:
+            if nodo.derecha is None:
+                nodo.derecha = Nodo(distancia, ciudades)
+            else:
+                self.insertar_recur(distancia, ciudades, nodo.derecha)
+        else:
+            nodo.ciudades += ciudades #si la distancia coincide lo añadimos a la lista de ciudades asociades con esta distancia
+
+    def in_order(self, nodo=None):
+        if nodo is None:
+            nodo = self.raiz
+
+        if nodo is not None:
+            self.in_order(nodo.izquierda)
+            print(f"Distancia: {nodo.distancia}, Ciudades: {nodo.ciudades}")
+            self.in_order(nodo.derecha)
+
+
--- claseGrafo.py
+++ claseGrafo.py
@@ -1,3 +1,4 @@
+from bst import NodoArbol, Arbol
 class Nodo:#Nodo
     def __init__(self,nombre):
         self.nombre=nombre
@@ -16,6 +17,7 @@ class Grafo:
     def __init__(self):
         self.numero_de_nodos = 0
         self.nodos = []
+        self.arbol_distancias = Arbol()
     
     def insertar_nodo(self,nodo):
         nodo.insertar_id(self.numero_de_nodos)
@@ -25,7 +27,9 @@ class Grafo:
     def insertar_arista(self, nodo1, nodo2, distancia):
         nodo1.caminos.append(Arista(nodo1, nodo2, distancia))
         nodo2.caminos.append(Arista(nodo2, nodo1, distancia))
-        
+        #Anadimos la distancia en nuestro arbol binario
+        self.arbol_distancias.insertar(distancia, [(nodo1.nombre, nodo2.nombre)])
+
     def mostrar_grafo(self):
         matriz_de_adyacencia = []
         for _ in range(self.numero_de_nodos):
@@ -38,7 +42,9 @@ class Grafo:
                 
         for fila in matriz_de_adyacencia:
             print(fila)
-        
+            
+    def mostrar_distancias_ordenadas(self):
+        self.arbol_distancias.in_order()    
 
 espania = Grafo()
 
@@ -52,6 +58,7 @@ espania.insertar_arista(ciudades[1],ciudades[3],994)
 
     
 espania.mostrar_grafo()
+espania.mostrar_distancias_ordenadas()
 
 
 
@@ -64,61 +71,3 @@ espania.mostrar_grafo()
 
 
 
-
-"""class NodoCamino:#camino entre dos ciudades
-    def __init__(self,distancia,ciudad_destino):
-        self.distancia=distancia
-        self.ciudad_destino=ciudad_destino
-        self.sig=None
-
-class Camino:#lista de todos los caminos que salen de una ciudad
-    def __init__(self):
-        self.inicio=None
-        self.tamanio=0
-
-    def insertar_camino(self,distancia,ciudad_destino):#método para insertar todos los caminos que hay desde una ciudad
-        nuevo=NodoCamino(distancia,ciudad_destino)
-        nuevo.sig=self.inicio
-        self.inicio=nuevo
-        self.tamanio+=1
-
-class Ciudad:#ciudad en nuestra red de transporte
-    def __init__(self,nombre):
-        self.nombre=nombre
-        self.sig=None
-        self.visitado=None
-        self.caminos= Camino()
-
-"Clase grafo que representa nuestra red de transporte entre ciudades"
-class Grafociudades:#grafo de nuestras ciudades unidas
-    def __init__(self,dirigido=True):
-        self.inicio=None
-        self.dirigido=dirigido
-        self.tamanio=0   
-
-    def insertar_ciudad(self,nombre):#función para crear una nueva ciudad en nuestro grafo
-        nueva_ciudad=Ciudad(nombre)
-        if self.inicio is None:
-            self.inicio=nueva_ciudad
-        else:
-            actual=self.inicio
-            while actual.sig is not None:
-                actual=actual.sig
-            actual.sig=nueva_ciudad
-        self.tamanio+=1
-
-    def insertar_camino(self,origen,destino,distancia):
-        ciudad_origen=None
-        ciudad_destino=None
-        act=self.inicio
-        while act is not None:
-            if act.nombre==origen:
-                ciudad_origen=act
-            elif act.nombre==destino:
-                ciudad_destino=act
-            act=act.sig
-        
-        if ciudad_origen is not None and ciudad_destino is not None:
-            ciudad_origen.caminos.insertar(distancia,ciudad_destino)
-            if not self.dirigido:# si no es dirigido puede haber camino de a->b pero no de b->a
-                ciudad_destino.caminos.insertar(distancia,ciudad_origen)"""
